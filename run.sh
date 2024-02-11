e2611dc60e6256e329ae8692b4c10c9475fd7074
Sanjeev Tyagi <34897955+sanjeevtyagi170@users.noreply.github.com>
2024-02-11 14:35:35 +0000

    spark config

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+pip install databricks.sdk
+pip install databricks
+pip install spark
+python import libraries
+python from pyspark.sql import SparkSession
+python -m pip install --upgrade pip
+ipython -i  /workspaces/Python-Codes/my_first_dab/src/notebook.ipynb
+# python /workspaces/Python-Codes/my_first_dab/src/notebook.ipynb
