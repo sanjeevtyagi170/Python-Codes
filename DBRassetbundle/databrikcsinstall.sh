f3ab3091afbc01a7095a8f2d3e28d04ed8179292
Sanjeev Tyagi <34897955+sanjeevtyagi170@users.noreply.github.com>
2024-02-11 14:36:16 +0000

    spark

--- DBRassetbundle/databrikcsinstall.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-curl -fsSL https://raw.githubusercontent.com/databricks/setup-cli/main/install.sh | sudo sh
--- DBRdevops/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# pip install databricks.sdk
-# pip install databricks
-# pip install spark
-# python import libraries
-# python from pyspark.sql import SparkSession
-python /workspaces/Python-Codes/DBRdevops/pyspark.py
