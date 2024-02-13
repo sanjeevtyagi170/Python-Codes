a3575161bcfc4fce222c834ee98dcbcd2b923bcd
Sanjeev Tyagi <34897955+sanjeevtyagi170@users.noreply.github.com>
2024-02-13 20:46:35 +0000

    a

--- test.py
+++ test.py
@@ -1,11 +1,8 @@
 import pandas as pd
 with open("input.txt","r") as fl:
     lines=fl.read().split("\n")
-numbers=[]
-text=[]
-for word in lines.split():
-    numbers.append(int(word[0]))
-    text.append(word[1])
+numbers=[int(line.split()[0]) for line in lines]
+text=[line.split()[1] for line in lines]
 df=pd.DataFrame({"numbers": numbers, "text": text})
 encoded_numbers=[]
 x=len(df["numbers"])
@@ -13,6 +10,6 @@ y=int(round((pow(x, 0.5045)*(1.341)),0))
 for i in range(y+2):
     encoded_numbers.append(int((i*(i+1)/2)))
 output=df[df["numbers"].isin(encoded_numbers)].sort_values(by="numbers")["text"].tolist()
-print(" ".join(output))
+# print(" ".join(output))
 
-
+print()
