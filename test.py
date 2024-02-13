33251f76db9a557de4eb40727b9dbfdd85b79ece
Sanjeev Tyagi <34897955+sanjeevtyagi170@users.noreply.github.com>
2024-02-13 11:51:46 +0000

    new files

--- test.py
+++ test.py
@@ -1,16 +1,4 @@
-import pandas as pd
-data= {'City Name' : ['Delhi','Mumbai','Bangalore','Hyderabad'],'City Population' : [100,100,200,300]
-}
- 
-df = pd.DataFrame(data)
-avg_population = []
-total_population = df['City Population'].sum()
-num_city=len(df)
- 
-for index, row in df.iterrows():
-    population_exc_city= total_population - row['City Population']
-    avg_population.append(population_exc_city/(num_city-1))
- 
-df['avg_pop'] = avg_population
-del df['City Population']
+df ['Start']=pd.to_datetime(df ['Start']) 
+df ['End']=pd.to_datetime(df ['End']) 
+df['diff']=(df ['End'] - df ['Start']).dt.total_seconds()/60
 print(df)
