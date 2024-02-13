73308cf4b26013fab8dcf529041d988cea59e322
Sanjeev Tyagi <34897955+sanjeevtyagi170@users.noreply.github.com>
2024-02-13 21:45:06 +0000

    a

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ def decode(message_file):
     text=[line.split()[1] for line in lines]
     no_of_rows=len(numbers)
     times_for_loop_run=int(round((pow(no_of_rows, 0.5045)*(1.341)),0)) # did by trial and error
+    print(times_for_loop_run,no_of_rows)
     encoded_numbers=[int((i*(i+1)/2)) for i in range(times_for_loop_run+2)]
     output = [text[i] for i in sorted(range(len(numbers)), key=numbers.__getitem__) if numbers[i] in encoded_numbers]
     print(" ".join(output))
