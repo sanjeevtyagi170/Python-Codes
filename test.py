f34ee1b8d6b60c2f484432dad7b1dd0e3544b535
Sanjeev Tyagi <34897955+sanjeevtyagi170@users.noreply.github.com>
2024-02-13 20:56:21 +0000

    a

--- test.py
+++ test.py
@@ -1,15 +1,15 @@
 import pandas as pd
-with open("input.txt","r") as fl:
-    lines=fl.read().split("\n")
-numbers=[int(line.split()[0]) for line in lines]
-text=[line.split()[1] for line in lines]
-df=pd.DataFrame({"numbers": numbers, "text": text})
-encoded_numbers=[]
-x=len(df["numbers"])
-y=int(round((pow(x, 0.5045)*(1.341)),0))
-for i in range(y+2):
-    encoded_numbers.append(int((i*(i+1)/2)))
-output=df[df["numbers"].isin(encoded_numbers)].sort_values(by="numbers")["text"].tolist()
-# print(" ".join(output))
+def decode(message_file):
+    with open(message_file,"r") as fl:
+        lines=fl.read().split("\n")
+    numbers=[int(line.split()[0]) for line in lines]
+    text=[line.split()[1] for line in lines]
+    dataframe=pd.DataFrame({"numbers": numbers, "text": text})
+    no_of_rows=dataframe.shape[0]
+    times_for_loop_run=int(round((pow(no_of_rows, 0.5045)*(1.341)),0)) # did by trial and error
+    encoded_numbers=[int((i*(i+1)/2)) for i in range(times_for_loop_run+2)]
+    output=dataframe[dataframe["numbers"].isin(encoded_numbers)].sort_values(by="numbers")["text"].tolist()
+    print(" ".join(output))
+    return " ".join(output)
 
-print()
+decode("input.txt")
