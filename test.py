b8a0ea793c4b0bbcfe794e43dc43b9b30d710f02
Sanjeev Tyagi <34897955+sanjeevtyagi170@users.noreply.github.com>
2024-02-13 21:23:31 +0000

    a

--- test.py
+++ test.py
@@ -1,11 +1,9 @@
-import pandas as pd
 def decode(message_file):
     with open(message_file,"r") as fl:
         lines=fl.read().split("\n")
     numbers=[int(line.split()[0]) for line in lines]
     text=[line.split()[1] for line in lines]
-    dataframe=pd.DataFrame({"numbers": numbers, "text": text})
-    no_of_rows=dataframe.shape[0]
+    no_of_rows=len(numbers)
     times_for_loop_run=int(round((pow(no_of_rows, 0.5045)*(1.341)),0)) # did by trial and error
     encoded_numbers=[int((i*(i+1)/2)) for i in range(times_for_loop_run+2)]
     output = [text[i] for i in sorted(range(len(numbers)), key=numbers.__getitem__) if numbers[i] in encoded_numbers]
