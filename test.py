98d6c00c24dc50016bfa08a6b8b845a49c5e91d8
Sanjeev Tyagi <34897955+sanjeevtyagi170@users.noreply.github.com>
2024-02-13 18:59:18 +0000

    a

--- test.py
+++ test.py
@@ -1,2 +1,13 @@
-myset={1,2,3,4,5}
-print(myset[2])
+import pandas as pd
+with open("input.txt","r") as fl:
+    file_read=fl.read()
+
+lines = file_read.split("\n")
+data=[]
+for line in lines:
+    words = line.split()
+    numbers = sum([int(word) for word in words if word.isdigit()])
+    text = " ".join([word for word in words if not word.isdigit()])
+    data.append({"numbers": numbers, "text": text})
+df=pd.DataFrame(data)
+print(df)
