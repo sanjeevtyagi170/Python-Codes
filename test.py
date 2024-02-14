04d642b557503d1cc1475a95d198d76359e116c7
Sanjeev Tyagi <34897955+sanjeevtyagi170@users.noreply.github.com>
2024-02-14 11:37:32 +0000

    a

--- test.py
+++ test.py
@@ -1,14 +1,12 @@
-def decode(message_file):
-    with open(message_file,"r") as fl:
-        lines=fl.read().split("\n")
-    numbers=[int(line.split()[0]) for line in lines]
-    text=[line.split()[1] for line in lines]
-    no_of_rows=len(numbers)
-    times_for_loop_run=int(round((pow(no_of_rows, 0.5045)*(1.341)),0)) # did by trial and error
-    print(times_for_loop_run,no_of_rows)
-    encoded_numbers=[int((i*(i+1)/2)) for i in range(times_for_loop_run+2)]
-    output = [text[i] for i in sorted(range(len(numbers)), key=numbers.__getitem__) if numbers[i] in encoded_numbers]
-    print(" ".join(output))
-    return " ".join(output)
+# def foo(x=[]):
+#     x.append(i)
+#     return x
+# for i in range(2):
+#     print(foo(i))
 
-decode("input.txt")
+def foo(i,x=[]):  # Create a new list inside the function
+  x.append(i)
+  return x
+
+for i in range(2):
+  print(foo(i))
