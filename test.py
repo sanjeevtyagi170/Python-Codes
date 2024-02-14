9bf46776f927796eaa288836a2f2fec430df8cd3
Sanjeev Tyagi <34897955+sanjeevtyagi170@users.noreply.github.com>
2024-02-14 11:56:34 +0000

    a

--- test.py
+++ test.py
@@ -1,9 +1,3 @@
-# def foo(x=[]):
-#     x.append(i)
-#     return x
-# for i in range(2):
-#     print(foo(i))
-
 def foo(i,x=[]):  # Create a new list inside the function
   x.append(i)
   return x
