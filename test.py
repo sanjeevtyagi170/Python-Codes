44b0e311988dc1120bc86b342b66a969c8bb11de
Sanjeev Tyagi <34897955+sanjeevtyagi170@users.noreply.github.com>
2024-02-15 16:11:42 +0000

    a

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
+import pandas as pd
 def foo(i,x=[]):  # Create a new list inside the function
   x.append(i)
   return x
 
 for i in range(2):
   print(foo(i))
+url="https://www.kaggle.com/datasets/wolfgang91/telecom-calls-dataset?select=ICalls+dataset.xlsx"
+df=pd.read_csv(url)
+print(df.head())
+  
