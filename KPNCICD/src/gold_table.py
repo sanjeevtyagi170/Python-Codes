ad4f21a689c1a46c82e55297f6530affa2467057
Sanjeev Tyagi <34897955+sanjeevtyagi170@users.noreply.github.com>
2024-02-16 12:22:48 +0000

    a

---
+++ KPNCICD/src/gold_table.py
@@ -0,0 +1,19 @@
+#Run the config file
+# %run ../<path to config file>
+
+#Import functions
+from pyspark.sql.functions import *
+
+
+# Read data from silver layer
+silver_df = spark.read.format("delta").load("<file_path>")
+
+#Join with other tables(optional)
+joined_df = silver_df.join(df2,on = "column1",how = "inner").select(silver_df.column1,df2.column2, ...)
+
+# Apply transformations and aggregations
+gold_df = joined_df.groupBy("id", "column1").agg(sum("column2").alias("column2"))
+
+# Write table to gold layer
+gold_df.write.format("delta").mode("overwrite").saveAsTable("catalog.schema.your_gold_table_name")
+
---
+++ KPNCICD/src/gold_table_with_dlt.py
@@ -0,0 +1,46 @@
+#run the config fie
+# %run /<notebook_path>
+
+#import required functions
+import dlt
+from pyspark.sql.types import *
+
+#define schema
+order_schema = StructType([
+    StructField('ORDER_ID',IntegerType(),False),
+    StructField('ORDER_DATETIME',StringType(),False),
+    StructField('CUSTOMER_ID',IntegerType(),False),
+    StructField('ORDER_STATUS',StringType(),False),
+    StructField('STORE_ID',IntegerType(),False)
+])
+
+#create silver tables
+@dlt_table
+def orders_silver():
+    df = spark.read.option('header',True).schema(order_schema).csv('file_path')
+    return df
+
+#Read silver tables and apply required transformation
+@dlt_table
+@dlt.expect("ORDER_ID_NOT_NULL","ORDER ID IS NOT NULL")
+def order_details_gold():
+    orders_silver = dlt.read('orders_silver')
+    order_items_silver = dlt.read('order_items_silver')
+    order_details = orders_silver.join(order_items_silver,ON = 'ORDER_ID','left')
+    order_details = order_details.select(orders_silver['ORDER_ID'],order_silver['ORDER_DATE'],order_silver['CUSTOMER_ID'],order_items_silver['PRODUCT_ID'],order_items_silver['UNIT_PRICE'],order_items_silver['QUANTITY'])
+    order_details = order_details.withColumn('SUB_TOTAL_AMOUNT',order_details['UNIT_PRICE']*order_details['QUANTITY'])
+    return order_details   
+	
+	
+#Aggregate the data
+@dlt_table
+def monthly_sales_gold():
+    order_details = dlt.read('order_details_gold')
+
+    monthly_sales = order_details.withColumn('MONTH_YEAR',date_format(order_details['ORDER_DATE'],'yyyy-MM'))
+    monthly_sales = monthly_sales.groupBy('MONTH_YEAR').sum('SUB_TOTAL_AMOUNT')
+    monthly_sales = monthly_sales.withColumn('TOTAL_SALES',round(sum('SUB_TOTAL_AMOUNT'),2).sort('MONTH_YEAR').desc())
+    monthly_sales = monthly_sales.select('MONTH_YEAR','TOTAL_SALES')
+
+   return monthly_sales 	
+	
