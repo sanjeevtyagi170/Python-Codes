b9524825964820877a99dc0fce77ef652fa89313
Sanjeev Tyagi <34897955+sanjeevtyagi170@users.noreply.github.com>
2024-02-17 06:57:42 +0000

    a

--- test.py
+++ test.py
@@ -1,14 +1,20 @@
 import pandas as pd
+
 df=pd.DataFrame({
     "country":["USA","India","UK","Germany","Australia"],
     "sales":[20000,25000,21000,8000,100000]
     })
-for i,row in df.iterrows():
-    if row["country"]=="India":
-        df.loc[i,"rank"]=1
-    else:
-        df.loc[i,"rank"]=0
-print(df.sort_values(by=["rank","sales"],ascending=[False,False]).drop("rank",axis=1))
+
+def ranking(df):
+    for i,row in df.iterrows():
+        if row["country"]=="India":
+            df.loc[i,"rank"]=1
+        else:
+            df.loc[i,"rank"]=0
+    return df.sort_values(by=["rank","sales"],ascending=[False,False]).drop("rank",axis=1)
+
+df1=df.where(df.)
+print(df1)
 
 # def calPoints(operations):
 #         operator=['+','C','D']
